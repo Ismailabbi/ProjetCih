5e58f66c3ec82bdf0c3d7b5c04817a6e3b1110b9
ismail-labbi <47818608+ismail-labbi@users.noreply.github.com>
2019-08-07 12:11:00 +0100

    ok

--- src/app/Services/dashbord.service.ts
+++ src/app/Services/dashbord.service.ts
@@ -5,8 +5,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DashbordService {
-  Url:string="http://localhost:9090/CodeIgniter/api/dashboard"
-  Url2:string="http://localhost:9090/CodeIgniter/api/dashboard_service"
+  Url:string="http://192.168.43.202:9090/CodeIgniter/api/dashboard"
+  Url2:string="http://192.168.43.202:9090/CodeIgniter/api/dashboard_service"
   constructor(private httpClient: HttpClient) { }
   get_dashbord1():Observable<any[]>{
     return this.httpClient.get<any[]>(this.Url)
@@ -16,11 +16,11 @@ export class DashbordService {
 }
 post_dashbord(a:number,b:number){
   console.log(b)
-  return this.httpClient.post('http://localhost:9090/CodeIgniter/api/test',{"Mois": b,"Annee":a})
+  return this.httpClient.post('http://192.168.43.202:9090/CodeIgniter/api/test',{"Mois": b,"Annee":a})
  
 }
 get_year():Observable<any[]>{
-  return this.httpClient.get<any[]>("http://localhost:9090/CodeIgniter/api/year")
+  return this.httpClient.get<any[]>("http://192.168.43.202:9090/CodeIgniter/api/year")
 }
 
 
